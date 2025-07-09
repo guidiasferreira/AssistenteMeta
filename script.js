623b965b7c89d716c08004c35fab1ce9de116cd3
Guilherme Dias Ferreira <gui.ferreira2606@gmail.com>
2025-07-08 21:00:25 -0300

    Assistente Meta

---
+++ script.js
@@ -0,0 +1,10 @@
+const apiKeyInput = document.querySelector('#apikey');
+const gameSelect = document.querySelector('#gameSelect');
+const questionInput = document.querySelector('#questionInput');
+const askButton = document.querySelector('#askButton');
+const aiResponse = document.querySelector('#aiResponse');
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', () => {
+    
+})
